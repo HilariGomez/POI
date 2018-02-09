e836dbe6be7b8e4ac847d936deb21ad19c112ac1
Hilari Gómez <josep-hilari.gomezviladot@worldline.com>
2018-02-09 14:29:35 +0100

    Email, web and phone in Poi Detail can be pressed and perform the pertinent action

--- app/src/main/java/com/example/a685559/poi/OnDetailActivity.java
+++ app/src/main/java/com/example/a685559/poi/OnDetailActivity.java
@@ -1,17 +1,22 @@
 package com.example.a685559.poi;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 public class OnDetailActivity extends AppCompatActivity implements InterestPointController.PointOfInterestListener {
 
-    TextView title, address, transport, email, description;
+    TextView title, address, transport, email, telephone, description;
+
+    TextView subTitleTransport;
+
+    InterestPoint poi;
 
     private SwipeRefreshLayout swipeRefreshLayout;
 
@@ -19,6 +24,9 @@ public class OnDetailActivity extends AppCompatActivity implements InterestPoint
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_detail);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefresh);
         swipeRefreshLayout.setOnRefreshListener(
@@ -42,36 +50,49 @@ public class OnDetailActivity extends AppCompatActivity implements InterestPoint
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.detail, menu);
+    public boolean onSupportNavigateUp() {
+        super.onBackPressed();
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_item_back) {
-            super.onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onPointDetailSuccess(InterestPoint poi) {
+        this.poi = poi;
         findViewById(R.id.loadingPanel).setVisibility(View.GONE);
 
         title = (TextView) findViewById(R.id.detailPoiTitle);
         address = (TextView) findViewById(R.id.detailPoiAddress);
+        subTitleTransport = (TextView) findViewById(R.id.transportSubTitle);
         transport = (TextView) findViewById(R.id.detailPoiTransport);
         email = (TextView) findViewById(R.id.detailPoiEmail);
+        telephone = (TextView) findViewById(R.id.detailPoiTelephone);
         description = (TextView) findViewById(R.id.detailPoiDescr);
 
         title.setPaintFlags(title.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         title.setText(poi.getTitle());
+        //getSupportActionBar().setTitle(poi.getTitle());
         address.setText(poi.getAddress());
-        transport.setText(poi.getTransport());
-        email.setText(poi.getEmail());
+
+        if (poi.getTransport().equals("null")) {
+            subTitleTransport.setVisibility(View.GONE);
+            transport.setVisibility(View.GONE);
+        } else {
+            transport.setText(poi.getTransport());
+        }
+
+        if (poi.getEmail().equals("null") || poi.getEmail().equals("undefined")) {
+            email.setVisibility(View.GONE);
+        } else {
+            email.setText(poi.getEmail());
+        }
+
+        if (poi.getPhone().equals("null") || poi.getPhone().equals("undefined")) {
+            telephone.setVisibility(View.GONE);
+        } else {
+            telephone.setText(poi.getPhone());
+        }
+
         //description.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
         description.setText(poi.getDescription());
     }
@@ -80,4 +101,5 @@ public class OnDetailActivity extends AppCompatActivity implements InterestPoint
     public void onError() {
 
     }
+    
 }
